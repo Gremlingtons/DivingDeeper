e1f86f5239ee6e77f018a5163e9c2d4fe4124a74
nhatv <nhatv2@gmail.com>
2023-08-21 23:15:25 -0400

    reworked controls, limited down presses

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,36 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float speed = 10f;
+    private int usage = 2; // number of uses when pressing down key
 
-    PlayerControls controls;
-    Vector2 move;
-    public float speed = 10;
-
-    private void Awake()
+    private void Start()
     {
-        controls = new PlayerControls();
-        controls.Player.Move.performed += context => move = context.ReadValue<Vector2>();
-        controls.Player.Move.canceled += context => move = Vector2.zero;
-    }
-
-    private void OnEnable()
-    {
-        controls.Player.Enable();
-    }
-
-    private void OnDisable()
-    {
-        controls.Player.Disable();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        Vector2 movement = new Vector2(move.x, move.y) * speed * Time.deltaTime;
-        transform.Translate(movement, Space.World);
+        // Horizontal movement
+        float moveX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveX * speed * Time.deltaTime, rb.velocity[1]);
+
+        // For pickup ability
+        if ((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && usage > 0)
+        {
+            rb.velocity = new Vector2(moveX * speed * Time.deltaTime, rb.velocity[1] * 2);
+            usage--;
+        }
     }
 }
